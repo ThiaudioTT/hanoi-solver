5c8174bd78ec4349a562db0fce6a2f2b20f30e09
ThiaudioTT <thiago.sky25@gmail.com>
2023-11-17 13:43:06 -0300

    feat: adds draw canvas

--- src/hanoi_solver/components/hanoi.cljs
+++ src/hanoi_solver/components/hanoi.cljs
@@ -1,5 +1,15 @@
 (ns hanoi-solver.components.hanoi
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [reagent.dom :as dom]))
+
+(defn clog [x] (js/console.log x)) ;; for debugging
+
+(defn draw-canvas-content [ canvas ] 
+  (let [ctx (.getContext canvas "2d")]
+    (.beginPath ctx)
+    (.moveTo ctx 0 0)
+    (.lineTo ctx 200 100)
+    (.stroke ctx)))
 
 (def canvas-style
   {:width 360
@@ -7,10 +17,21 @@
    :style {:border "1px solid black"}})
 
 (defn tower-of-hanoi []
-  (let [dom-node (reagent/atom nil)
-        discs (reagent/atom 3)]
+  (let [hanoi-canvas (reagent/atom nil) discs (reagent/atom 3)]
     (reagent/create-class
-     {:reagent-render
+     {
+    ;;   :component-did-update
+    ;;   (fn [this]
+    ;;     (clog "component-did-update")
+    ;;     (draw-canvas-content (.-firstChild @hanoi-canvas)))
+
+      :component-did-mount
+      (fn [this]
+        (clog (dom/dom-node this))
+        (reset! hanoi-canvas (.-firstChild (dom/dom-node this)))
+        (draw-canvas-content @hanoi-canvas))
+
+      :reagent-render
       (fn []
         [:div.tower-of-hanoi
          [:canvas canvas-style]])})))
