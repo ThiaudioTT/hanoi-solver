281967823530f1ca473758909c7bd12e18ee0b09
ThiaudioTT <thiago.sky25@gmail.com>
2023-11-17 14:51:54 -0300

    feat: draw towers

--- src/hanoi_solver/components/hanoi.cljs
+++ src/hanoi_solver/components/hanoi.cljs
@@ -4,23 +4,41 @@
 
 (defn clog [x] (js/console.log x)) ;; for debugging
 
-(defn draw-canvas-content [ canvas ] 
-  (let [ctx (.getContext canvas "2d")]
-    (.beginPath ctx)
-    (.moveTo ctx 0 0)
-    (.lineTo ctx 200 100)
-    (.stroke ctx)))
+
+;; global vars 
+
+(def width 360)
+(def height 250)
+(def tower-width 15)
+(def tower-height (/ height 2))
+(def tower-edge-spacing 40)
+(def tower-color "black")
 
 (def canvas-style
-  {:width 360
-   :height 250
+  {:width width
+   :height height
    :style {:border "1px solid black"}})
+
+;; -------------------------
+;; main program
+
+;; args are canvas, context, x, y
+(defn draw-tower [ctx x y]
+  (.fillRect ctx x y tower-width tower-height))
+
+(defn draw-towers [ctx]
+  (draw-tower ctx tower-edge-spacing tower-height)
+  (draw-tower ctx (/ width 2) tower-height)
+  (draw-tower ctx (- width tower-edge-spacing tower-width) tower-height))
+
+(defn draw-canvas-content [canvas]
+  (let [ctx (.getContext canvas "2d")]
+    (draw-towers ctx)))
 
 (defn tower-of-hanoi []
   (let [hanoi-canvas (reagent/atom nil) discs (reagent/atom 3)]
     (reagent/create-class
-     {
-    ;;   :component-did-update
+     {;;   :component-did-update
     ;;   (fn [this]
     ;;     (clog "component-did-update")
     ;;     (draw-canvas-content (.-firstChild @hanoi-canvas)))
